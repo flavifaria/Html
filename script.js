f3a66deb173ecf84679a39c55b5bb9369b6c8037
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2025-05-21 12:13:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function atualizarRelogio() {
     const relogio = document.getElementById('relogio')
     //Aqui eu vou mexer no elemento com ID 'relógio' no HTML
     const agora = new Date();
-    agora.setHours(agora.getHours()-3)
     //Essa linha serve para definir para o fuso horário correto
     const horas = String(agora.getHours()).padStart(2,'0');
     const minutos = String(agora.getMinutes()).padStart(2,'0');
